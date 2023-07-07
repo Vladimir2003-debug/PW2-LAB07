37df4cb65550a896ef7d216a4e1688524d81a8c3
Vladimir Arturo Sulla Quispe <vsullaq@unsa.edu.pe>
2023-07-07 18:46:19 -0500

    registrado en el admin

--- model_examples/apps/examples/admin.py
+++ model_examples/apps/examples/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Simple,DateExample,NullExample,Language,Framework
 
 # Register your models here.
+
+admin.site.register(Simple)
+admin.site.register(DateExample)
+admin.site.register(NullExample)
+admin.site.register(Language)
+admin.site.register(Framework)
