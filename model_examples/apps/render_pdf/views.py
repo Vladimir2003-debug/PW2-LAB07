5a4e246a0c1627c12aebcefbe3645f147afc0d2e
Vladimir Arturo Sulla Quispe <vsullaq@unsa.edu.pe>
2023-07-08 13:03:08 -0500

    añadido funcion para descargar

--- model_examples/apps/render_pdf/views.py
+++ model_examples/apps/render_pdf/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import View
+from django.template.loader import get_template
+from .utils import render_to_pdf #created in step 4
 
-# Create your views here.
+class GeneratePdf(View):
+    def get(self, request, *args, **kwargs):
+        template = get_template('pdf/invoice.html')
+        context = {
+            'invoice_id': 123, 
+            'customer_name': 'Vladimir Sulla',
+            'amount': 12000,
+            'today': "Today",
+        }
+        pdf = render_to_pdf('pdf/invoice.html',context)
+        return HttpResponse(pdf, content_type='application/pdf')
