9f42727cc0f9d517ce0da4ee9c25398fe38000e8
Vladimir Arturo Sulla Quispe <vsullaq@unsa.edu.pe>
2023-07-08 13:06:41 -0500

    eliminado include

--- model_examples/apps/render_pdf/urls.py
+++ model_examples/apps/render_pdf/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .views import GeneratePdf
-from django.urls import path,include
+from django.urls import path
 
 urlpatterns = [
     path('',GeneratePdf.as_view(), name="pdf"),
