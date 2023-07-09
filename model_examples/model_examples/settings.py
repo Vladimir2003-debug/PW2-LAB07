2dc298d8ab623626973919f7551151846e1b33c5
Vladimir Arturo Sulla Quispe <vsullaq@unsa.edu.pe>
2023-07-08 23:13:52 -0500

    tratando de que se envie correos por django

--- model_examples/model_examples/settings.py
+++ model_examples/model_examples/settings.py
@@ -129,3 +129,14 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email config 
+#
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+ACCOUNT_EMAIL_VERIFICATION = 'none'
