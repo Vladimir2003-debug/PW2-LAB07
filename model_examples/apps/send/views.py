b8041696c4ebfbaf9c0acf820aaf35a7164108e9
Vladimir Arturo Sulla Quispe <vsullaq@unsa.edu.pe>
2023-07-08 23:13:30 -0500

    cambiado la forma en la que enviar send email

--- model_examples/apps/send/views.py
+++ model_examples/apps/send/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.core.mail import send_mail
 # Create your views here.
+
+def index(request):
+    send_mail(
+        "Hello from Vladimir Company",               # Asunto del email
+        "Hello there, this is an automatic message", # Mensaje en el email
+        "vsullaq@unsa.edu.com",                        # El correo que envia
+        ["undertaker2003.nd@gmail.com","linoy54385@lukaat.com"],                  # los correos a los que
+
+        )
+    return render(request, 'send/index.html')
