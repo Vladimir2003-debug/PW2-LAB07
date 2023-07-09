0f48a908a3a7f7bfc5f7f9f44dd8e5a95bcd51a2
alumno <alumno@ubuntu.mail.org>
2023-07-09 17:45:10 -0500

    modificado para envio de email usando postfix

--- model_examples/model_examples/settings.py
+++ model_examples/model_examples/settings.py
@@ -133,10 +133,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Email config 
 #
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 25
 EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
-EMAIL_USE_TLS = True
+EMAIL_USE_TLS = False
 EMAIL_USE_SSL = False
 ACCOUNT_EMAIL_VERIFICATION = 'none'
