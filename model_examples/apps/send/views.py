2f713ef95b0bc1c3790e5e5b1f633d64c0a52e54
alumno <alumno@ubuntu.mail.org>
2023-07-09 17:44:19 -0500

    modificado el email que envia

--- model_examples/apps/send/views.py
+++ model_examples/apps/send/views.py
@@ -6,8 +6,8 @@ def index(request):
     send_mail(
         "Hello from Vladimir Company",               # Asunto del email
         "Hello there, this is an automatic message", # Mensaje en el email
-        "vsullaq@unsa.edu.com",                        # El correo que envia
-        ["undertaker2003.nd@gmail.com","linoy54385@lukaat.com"],                  # los correos a los que
-
+        "vladimir@unsa.com",                        # El correo que envia
+        ["linoy54385@lukaat.com"],                  # los correos a los que
+        fail_silently=False,
         )
     return render(request, 'send/index.html')
