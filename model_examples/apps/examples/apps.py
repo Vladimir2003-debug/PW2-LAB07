9b951b4d027224a0c4a14ac9d7d89f2037507997
Vladimir Arturo Sulla Quispe <vsullaq@unsa.edu.pe>
2023-07-08 23:12:36 -0500

    cambiado conpre de apps en exampes

--- model_examples/apps/examples/apps.py
+++ model_examples/apps/examples/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ExamplesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.apps.examples'
+    name = 'apps.examples'
