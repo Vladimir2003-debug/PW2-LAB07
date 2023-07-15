b5ba70e0fbcbe545e96db4187e2668cf4fee562f
Vladimir Arturo Sulla Quispe <vsullaq@unsa.edu.pe>
2023-07-14 21:33:13 -0500

    tanta cosa por un boton xd

--- model_examples/apps/send/views.py
+++ model_examples/apps/send/views.py
@@ -7,13 +7,15 @@ def index(request):
     message = request.POST['message']
     print(message)
     print(email)
+    """
     send_mail(
-        "Hello from Vladimir Company",               # Asunto del email
-        message, # Mensaje en el email
+        "Hello from Vladimir Company",              # Asunto del email
+        message,                                    # Mensaje en el email
         "vladimir@unsa.com",                        # El correo que envia
-        [email],                  # los correos a los que
+        [email],                                    # los correos a los que
         fail_silently=False,
         )
+    """
     return render(request, 'send/index.html')
 
 def index_view(request):
